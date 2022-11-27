6a86993f04ff6a06fed6a25b85984915244efa15
Raghavendra <raghavendrardeshabhandari@gmail.com>
2022-11-27 15:20:28 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,118 @@
 import logo from './logo.svg';
 import './App.css';
+import './Navbar.css';
+import { Card, CardHeader, CardBody, CardFooter,Image, Stack, Heading ,Text, Button } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      Hello world!
+    <div className="App" >
+      <header id='header' >
+        <div className='container' >
+          <nav className='main-nav'> 
+            <a className='normalLink' href='#Home' >Home</a>
+            <a className='normalLink' href='#About' >About Me</a>
+            <a className='normalLink' href='#Skills' >Skills</a>
+            <a className='normalLink' href='#Project' >Project</a>
+            <a className='normalLink' href='#Contact' >Contact</a>
+            <a className='normalLink' href='#Resume' >Resume</a>
+          </nav>
+        </div>
+      </header>
+        <div>
+          <section className='sections' id='Home'>
+            <h1>Home</h1>
+            <div>
+              <Card
+                direction={{ base: 'column', sm: 'row' }}
+                overflow='hidden'
+                variant='outline'
+                style={{
+                  color:'white',
+                  width:'90%',
+                  margin:'auto'
+                }}
+              >
+                <Image
+                  objectFit='cover'
+                  maxW={{ base: '100%', sm: '200px' }}
+                  src='https://avatars.githubusercontent.com/u/107551502?v=4'
+                  alt='Profile Pic'
+                />
+
+                <Stack>
+                  <CardBody>
+                    <Heading size='md'>Raghavendra RD</Heading>
+
+                    <Text py='2'>
+                    Proficient in translating designs  into high-quality code. Troubleshooting & debugging to improve overall performance with problem solving skills.
+                    </Text>
+                  </CardBody>
+
+                  <CardFooter>
+                    <Button variant='solid' colorScheme='blue'>
+                      More info
+                    </Button>
+                  </CardFooter>
+                </Stack>
+              </Card>
+            </div>
+          </section>
+          <section  className='sections'id='About'>
+            <h1>About Me</h1>
+            <p>Contrary to popular belief, Lorem Ipsum is not
+              simply random text. It has roots in 
+              a piece of classical Latin literature from
+                45 BC, making it over 2000 years old. Richard 
+                McClintock, a Latin professor at Hampden-Sydney 
+                College in Virginia, looked up one of the more ob
+                scure Latin words, consectetur, from a Lorem Ipsum 
+            </p>
+          </section>
+          <section  className='sections'id='Skills'>
+            <h1>Skills</h1>
+            <p>Contrary to popular belief, Lorem Ipsum is not
+              simply random text. It has roots in 
+              a piece of classical Latin literature from
+                45 BC, making it over 2000 years old. Richard 
+                McClintock, a Latin professor at Hampden-Sydney 
+                College in Virginia, looked up one of the more ob
+                scure Latin words, consectetur, from a Lorem Ipsum 
+            </p>
+          </section>
+          <section  className='sections'id='Project'>
+            <h1>Project</h1>
+            <p>Contrary to popular belief, Lorem Ipsum is not
+              simply random text. It has roots in 
+              a piece of classical Latin literature from
+                45 BC, making it over 2000 years old. Richard 
+                McClintock, a Latin professor at Hampden-Sydney 
+                College in Virginia, looked up one of the more ob
+                scure Latin words, consectetur, from a Lorem Ipsum 
+            </p>
+          </section>
+          <section  className='sections'id='Contact'>
+            <h1>Contact</h1>
+            <p>Contrary to popular belief, Lorem Ipsum is not
+              simply random text. It has roots in 
+              a piece of classical Latin literature from
+                45 BC, making it over 2000 years old. Richard 
+                McClintock, a Latin professor at Hampden-Sydney 
+                College in Virginia, looked up one of the more ob
+                scure Latin words, consectetur, from a Lorem Ipsum 
+            </p>
+          </section>
+          <section  className='sections'id='Resume'>
+            <h1>Resume</h1>
+            <p>Contrary to popular belief, Lorem Ipsum is not
+              simply random text. It has roots in 
+              a piece of classical Latin literature from
+                45 BC, making it over 2000 years old. Richard 
+                McClintock, a Latin professor at Hampden-Sydney 
+                College in Virginia, looked up one of the more ob
+                scure Latin words, consectetur, from a Lorem Ipsum 
+            </p>
+          </section>
+       </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.body.style.backgroundColor = 'black';
+document.body.style.color = 'white' ;  // '#00ff00';
+console.log('styles:',document.body.style.backgroundColor, document.body.style.color)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <ChakraProvider>
+      <App/>
+    </ChakraProvider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
